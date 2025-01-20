f0b51af9e43ad12d99810b2879fe0092e5566c7b
AmrHani <117240613+Amr-Hani@users.noreply.github.com>
2025-01-20 19:16:25 +0200

    update mulFun branch

--- math.js
+++ math.js
@@ -4,12 +4,11 @@ function display() {
   console.log("vero , amr , alaa, 3alaa , marina");
 }
 
-function mulFn(x, y) {
-  return x * y;
-
+function mulFn(x, y, z) {
+  return x * y * z;
+}
 function add(a, b) {
   if (typeof a != "number" || typeof b != "number")
     throw "please enter only tow number";
   return a + b;
 }
-
